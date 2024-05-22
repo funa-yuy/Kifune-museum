3a56d97937049aae200372edefc780e4302867b7
funa <orz.home35@gmail.com>
2024-05-22 17:00:56 +0900

    240522_hum-js修正

--- main.js
+++ main.js
@@ -3,22 +3,17 @@
 const ham = document.querySelector('#js-hamburger');
 const nav = document.querySelector('#js-nav');
 
-/*
-ham.addEventListener('click', function () {
-
-  ham.classList.toggle('active');
-  nav.classList.toggle('active');
-
-});
-*/
-
 
 document.getElementById('js-hamburger').addEventListener('click',function() {
 document.getElementById('js-nav').classList.toggle('active');
 });
 
-document.getElementById('rink').addEventListener('click',function() {
-document.getElementById('js-nav').classList.remove('active');
+$('a[href*="#"]').click(function () {
+	document.getElementById('js-nav').classList.remove('active');
+	var elmHash = this.getAttribute('href'); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
+	var pos = $(elmHash).offset().top - 30;  //idの上部の距離を取得
+	$('html, body').animate({scrollTop: pos}, 500); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
+	return false;
 });
 
 // ヘッダー登場
@@ -30,13 +25,3 @@ $(window).scroll(function () {           /* スクロールされた時 */
     $('header').fadeOut();               /* ヘッダーをふわっと非表示 */
   }
 });
-
-
-//  ページ内リンク　スクロール
- $('a[href*="#"]').click(function () {
-	var elmHash = $(this).attr('href'); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
-	var pos = $(elmHash).offset().top - 50;  //idの上部の距離を取得
-	$('body,html').animate({scrollTop: pos}, 500); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
-	return false;
-  });
-
